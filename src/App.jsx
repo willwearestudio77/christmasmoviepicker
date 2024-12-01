074db35841609d6f110244556ca7c28546c2ea1c
David Hutcheson <d.hutcheson@hotmail.co.uk>
2024-12-01 13:41:57 +0000

    Age filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ApiCall from "./components/apicall";
+import Age from "./components/Age";
 
 function App() {
   return (
     <div>
       <h1>Welcome to the Pickr</h1>
       <ApiCall />
+      <Age />
     </div>
   );
 }
